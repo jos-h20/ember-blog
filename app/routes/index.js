eadc7bc988946988f09847f1016036634a6de469
Josh Overly <joshoverly@yahoo.com>
2016-04-05 16:54:31 -0700

    fix display

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.findAll('post');
+    return this.store.query('post', {
+      limitToLast: 6
+    });
   }
 });
